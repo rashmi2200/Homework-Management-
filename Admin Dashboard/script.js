14902212643ace40513dbdc8a396f6acdcba34b2
Rashmi Dhungana <rashmidhngn@gmail.com>
2024-04-09 15:28:14 +0545

    first_commit

---
+++ Admin Dashboard/script.js
@@ -0,0 +1,12 @@
+const menuItems = document.querySelectorAll(".main-menu li");
+
+menuItems.forEach((menuItem) => {
+  const subMenu = menuItem.querySelector("ul");
+  menuItem.addEventListener("click", () => {
+    menuItems.forEach((item) => item.classList.remove("active"));
+    menuItem.classList.add("active");
+    if (subMenu) {
+      subMenu.classList.toggle("active");
+    }
+  });
+});
